3874aa5669e7eb4392091750ad5acd796593574e
Michael Jungo <michaeljungo92@gmail.com>
2022-08-02 17:55:58 +0200

    Use `vim.ui.input` instead of `vim.fn.input` for prompts
    Makes it nicer, as things like dressing.nvim can show a prettier prompt
    instead of having the one at the bottom.

--- .config/nvim/lua/utils/dap.lua
+++ .config/nvim/lua/utils/dap.lua
@@ -3,44 +3,52 @@ local dap = require("dap")
 local M = {}
 
 function M.set_conditional_breakpoint(condition)
-  -- Ask user to input a condition if none was given
-  if not condition then
-    condition = vim.trim(vim.fn.input("Condition: "))
+  if condition then
+    dap.set_breakpoint(condition)
+  else
+    -- Ask user to input a condition if none was given
+    vim.ui.input({ prompt = "Condition" }, function(input)
+      input = vim.trim(input or "")
+      -- Do not set the breakpoint if the condition is empty
+      if input ~= "" then
+        dap.set_breakpoint(input)
+      end
+    end)
   end
-  -- Do not set the breakpoint if the condition is empty
-  if condition == "" then
-    return
-  end
-  dap.set_breakpoint(condition)
 end
 
 function M.set_hit_count_breakpoint(count)
-  -- Ask user to input the hit count if none was given
   if count then
     -- Ensure that it's a string, because the hit count is more than a number
     -- for example >2 would stop for every step after hitting it twice.
     -- For convenience, so that the function can be called with just a number.
     count = tostring(count)
+    dap.set_breakpoint(nil, count)
   else
-    count = vim.trim(vim.fn.input("Hit Count: "))
+    -- Ask user to input the hit count if none was given
+    vim.ui.input({ prompt = "Hit Count" }, function(input)
+      input = vim.trim(input or "")
+      -- Do not set the breakpoint if the given count is empty
+      if input ~= "" then
+        dap.set_breakpoint(nil, input)
+      end
+    end)
   end
-  -- Do not set the breakpoint if the given count is empty
-  if count == "" then
-    return
-  end
-  dap.set_breakpoint(nil, count)
 end
 
 function M.set_log_point(message)
-  -- Ask user to input a message if none was given
-  if not message then
-    message = vim.trim(vim.fn.input("Log Message: "))
+  if message then
+    dap.set_breakpoint(nil, nil, message)
+  else
+    -- Ask user to input a message if none was given
+    vim.ui.input({ prompt = "Log Message" }, function(input)
+      input = vim.trim(input or "")
+      -- Do not set the log point if the message is empty
+      if input ~= "" then
+        dap.set_breakpoint(nil, nil, input)
+      end
+    end)
   end
-  -- Do not set the log point if the message is empty
-  if message == "" then
-    return
-  end
-  dap.set_breakpoint(nil, nil, message)
 end
 
 return M
