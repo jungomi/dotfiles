b11d3d41d553e062331157de83eaa7f7faa96be4
Michael Jungo <michaeljungo92@gmail.com>
2023-02-16 22:14:13 +0100

    Ensure autocommands are loaded after plugins
    Otherwise the they are not available.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("options")
 require("mappings")
-require("autocommands")
 require("theme").load_theme()
 -- Load plugins
 require("load_plugins")
+-- Autocommands need to be loaded after the plugins are loaded, as they import them.
+require("autocommands")
