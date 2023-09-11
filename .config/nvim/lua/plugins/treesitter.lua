ba78fb0a97c8c77ad7487b8ead147cf70ca8cc35
Michael Jungo <michaeljungo92@gmail.com>
2023-09-11 15:24:18 +0200

    feat(nvim): automatically convert interpolation quotes
    This plugin converts regular quotes to interpolated quotes (including
    f-strings for Python) whenever the interpolation is used and back once
    there is no interpolation left.
    
    Example
    
    ```py
    # Regular string with the cursor at |
    msg = "Starting |"
    
    # Typing: {job} results in:
    msg = f"Starting {job}"
    ```

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,4 +12,16 @@ return {
       "danymat/neogen",
     },
   },
+  {
+    -- Automatically convert regular quotes to interpolated versions (including f-strings for Python)
+    "axelvc/template-string.nvim",
+    opts = {
+      -- Convert to regular quotes when there is no interpolation
+      remove_template_string = true,
+      restore_quotes = {
+        -- Use double quotes for regular quotes
+        normal = [["]],
+      },
+    },
+  },
 }
