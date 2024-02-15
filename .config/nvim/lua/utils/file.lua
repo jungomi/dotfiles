b50e538d8ac7cd7717a15341cd60ec1a9a091f5e
Michael Jungo <michaeljungo92@gmail.com>
2024-02-15 15:06:08 +0100

    fix(nvim): avoid trying to create a directory for protocols
    Any protocol (e.g. fugitive://) does not refer to a local file, and
    sometimes it allows you to save that buffer, which will be handled by
    the plugin, so the parent directory should not be created.

--- .config/nvim/lua/utils/file.lua
+++ .config/nvim/lua/utils/file.lua
@@ -3,6 +3,11 @@ local fn = vim.fn
 local M = {}
 
 function M.mkdir_on_save()
+  local path = vim.api.nvim_buf_get_name(0)
+  if path:find("://") then
+    -- Any file with a protocol (e.g. fugitive://) is ignored, since it's not a directory to create.
+    return
+  end
   local dir = fn.expand("%:p:h")
   if fn.isdirectory(dir) == 0 then
     -- Ask if the directory should be created
