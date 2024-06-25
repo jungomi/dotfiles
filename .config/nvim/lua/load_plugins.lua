c2509195f05d11fffd148cea1d1bb47c3949d79c
Michael Jungo <michaeljungo92@gmail.com>
2024-06-25 15:20:24 +0200

    chore(nvim): upgrade to Lazy.nvim 11.0 and all plugins

--- .config/nvim/lua/load_plugins.lua
+++ .config/nvim/lua/load_plugins.lua
@@ -2,20 +2,26 @@ local icons = require("icons")
 local borders = require("borders")
 
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazy_path) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazy_path,
-  })
+if not vim.uv.fs_stat(lazy_path) then
+  vim
+    .system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazy_path,
+    })
+    :wait()
 end
 vim.opt.rtp:prepend(lazy_path)
 
 local lazy = require("lazy")
-lazy.setup("plugins", {
+lazy.setup({
+  spec = {
+    -- Plugins are in defined in the plugins/ directory
+    { import = "plugins" },
+  },
   install = {
     colorscheme = { "mine" },
   },
--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -2,7 +2,13 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "mfussenegger/nvim-dap-python",
+      {
+        "mfussenegger/nvim-dap-python",
+        -- Need to disable the build, because Lazy 11.0 now tries to build the plugin if there is a rockspec.
+        -- But this plugin has rockspec that is not for the plugin but mainly for testing, so it cannot be installed
+        -- with it, therefore disable trying to build it.
+        build = false,
+      },
       "theHamsta/nvim-dap-virtual-text",
       "nvim-neotest/nvim-nio",
       "rcarriga/nvim-dap-ui",
