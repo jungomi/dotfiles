55dbc70e05fb2b0d4a3e0b5ed667363ad4e669b0
Michael Jungo <michaeljungo92@gmail.com>
2024-07-22 15:09:03 +0200

    feat(nvim): add render markdown plugin

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -91,4 +91,30 @@ return {
       },
     },
   },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- Mandatory
+      "nvim-tree/nvim-web-devicons", -- Optional but recommended
+    },
+    opts = {
+      -- table_style = "normal",
+      heading = {
+        backgrounds = {},
+      },
+      code = {
+        highlight = "CursorLine",
+      },
+      dash = {
+        highlight = "Title",
+      },
+      pipe_table = {
+        row = "Title",
+      },
+      link = {
+        highlight = "Comment",
+      },
+    },
+  },
 }
