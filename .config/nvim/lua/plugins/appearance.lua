c9b66854fe5bca6327022b99649c0f2fd2d4e3ca
Michael Jungo <michaeljungo92@gmail.com>
2024-07-22 13:22:02 +0200

    fix: don't trigger which-key when entering visual mode

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -78,6 +78,9 @@ return {
         end
         return delay
       end,
+      defer = function(ctx)
+        return vim.list_contains({ "<C-V>", "V", "v" }, ctx.mode)
+      end,
       plugins = {
         spelling = {
           enabled = true,
