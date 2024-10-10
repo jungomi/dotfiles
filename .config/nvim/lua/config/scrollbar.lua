d74e19d755ba9d18322d0c57605f375f36083e6d
Michael Jungo <michaeljungo92@gmail.com>
2024-10-10 16:31:36 +0200

    chore(nvim): upgrade plugins and fix scrollview
    Scrollview broke the gitsigns integration I used from a Gist, but now
    has an integrated support for it, that you can just call instead.

--- .config/nvim/lua/config/scrollbar.lua
+++ .config/nvim/lua/config/scrollbar.lua
@@ -1,85 +1,9 @@
 local scrollview = require("scrollview")
+local scrollview_gitsigns = require("scrollview.contrib.gitsigns")
 local scrollbar_mappings = require("mappings.scrollbar")
 local icons = require("icons")
 
 local M = {}
-
--- Taken from https://gist.github.com/dstein64/b5d9431ebeacae1fb963efc3f2c94cf4
-local function setup_gitsigns()
-  local group = "gitsigns"
-
-  local add = scrollview.register_sign_spec({
-    group = group,
-    highlight = "GitSignsAdd",
-    symbol = icons.right_bar,
-    priority = 80,
-  }).name
-
-  local change = scrollview.register_sign_spec({
-    group = group,
-    highlight = "GitSignsChange",
-    symbol = icons.right_bar,
-    priority = 80,
-  }).name
-
-  local delete = scrollview.register_sign_spec({
-    group = group,
-    highlight = "GitSignsDelete",
-    symbol = icons.right_bar,
-    priority = 80,
-  }).name
-
-  scrollview.set_sign_group_state(group, true)
-
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "ScrollViewRefresh",
-    callback = function()
-      if not scrollview.is_sign_group_active(group) then
-        return
-      end
-      local success, gitsigns = pcall(require, "gitsigns")
-      if not success then
-        return
-      end
-      for _, winid in ipairs(scrollview.get_sign_eligible_windows()) do
-        local bufnr = vim.api.nvim_win_get_buf(winid)
-        local hunks = gitsigns.get_hunks(bufnr) or {}
-        local lines_add = {}
-        local lines_change = {}
-        local lines_delete = {}
-        for _, hunk in ipairs(hunks) do
-          if hunk.type == "add" then
-            -- Don't show if the entire column would be covered.
-            if hunk.added.count < vim.api.nvim_buf_line_count(bufnr) then
-              for line = hunk.added.start, hunk.added.start + hunk.added.count - 1 do
-                table.insert(lines_add, line)
-              end
-            end
-          elseif hunk.type == "change" then
-            for line = hunk.added.start, hunk.added.start + hunk.added.count - 1 do
-              table.insert(lines_change, line)
-            end
-          elseif hunk.type == "delete" then
-            table.insert(lines_delete, hunk.added.start)
-          end
-        end
-        vim.b[bufnr][add] = lines_add
-        vim.b[bufnr][change] = lines_change
-        vim.b[bufnr][delete] = lines_delete
-      end
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "GitSignsUpdate",
-    callback = function()
-      if not scrollview.is_sign_group_active(group) then
-        return
-      end
-      scrollview.refresh()
-    end,
-  })
-end
 
 function M.setup()
   scrollview.setup({
@@ -97,7 +21,11 @@ function M.setup()
     spell_symbol = icons.pad_right(icons.lsp_kind.Text, 1),
   })
 
-  setup_gitsigns()
+  scrollview_gitsigns.setup({
+    add_symbol = icons.right_bar,
+    change_symbol = icons.right_bar,
+    delete_symbol = icons.right_bar,
+  })
 
   scrollbar_mappings.enable_mappings()
 end
