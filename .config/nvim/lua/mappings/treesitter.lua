524475877ea49b72dfac2df26494f044eb0f3d17
Michael Jungo <michaeljungo92@gmail.com>
2024-11-23 03:49:31 +0100

    feat(nvim): create dedicatd mapping to jump start/end of tresitter node
    I had it integrated with the snippet jumps, but occasionally I triggered
    it accidentally because I though I still had a jump left. So a dedicated
    mapping is a better idea, and I noticed the Mac (or emacs) binds for
    start/end of line were sort of free in insert mode (they have something
    completely useless by default).

--- .config/nvim/lua/mappings/treesitter.lua
+++ .config/nvim/lua/mappings/treesitter.lua
@@ -1,11 +1,15 @@
+local ts_utils = require("utils.treesitter")
 local map_utils = require("utils.map")
 local nmap = map_utils.nmap
+local imap = map_utils.imap
 
 local M = {}
 
 function M.enable_mappings()
   -- Generate DocString of the object under the cursor
   nmap("<leader>sd", "<Cmd>Neogen<CR>", { desc = "Treesitter » Generate DocString" })
+  imap("<C-a>", ts_utils.jump_start_of_node, { desc = "Treesitter » Jump to start of Node" })
+  imap("<C-e>", ts_utils.jump_end_of_node, { desc = "Treesitter » Jump to end of Node" })
 end
 
 return M
